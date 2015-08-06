163dc88b4b5232e5f398d625476b7b179b014b13
mjsgreen01 <mjsgreen01@gmail.com>
2015-08-06 16:35:37 -0700

    [feat] add gulp task - minify and prefix css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
-  stylish = require('jshint-stylish');
+  stylish = require('jshint-stylish'),
+  autoprefix = require('gulp-autoprefixer'),
+  minifyCSS = require('gulp-minify-css');
 
 
 
@@ -9,4 +11,12 @@ gulp.task('lint', function(){
     .src(['./public/app/**/*.js', './app.js', './controller/*.js', './models/*.js', './middlewares/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('styles', function(){
+  return gulp
+    .src('./public/assets/css/*.css')
+    .pipe(autoprefix(['> 1%', 'last 3 versions', 'Firefox ESR', 'Opera 12.1']))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./public/assets/css/min/'));
 });
