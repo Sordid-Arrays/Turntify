9b5751bcb38ce2e441ac3dfdc6476edcfe72d7d6
Shu Yoshioka <yoshioka63@gmail.com>
2015-08-07 09:15:33 +0900

    use queryString

--- middlewares/echonest.js
+++ middlewares/echonest.js
@@ -1,29 +1,31 @@
 /*
  * for accessing Echonest API
 **/
+var queryString = require('query-string');
 var request = require('request');
+
 var config = require('../config');
 
 /*
  * getting Echo Nest track data
 **/
 var getTrackData = function (spotyfyId) {
+  var query = queryString.stringify({
+    api_key: config.ECHONEST_API_KEY,
+    format: "json",
+    results: 1,
+    bucket: 'audio_summary',
+    track_id: spotyfyId
+  });
+
   request({
     method: 'GET',
-    url: 'http://developer.echonest.com/api/v4/song/profile', //URL to hit
-    qs: {
-      api_key: config.ECHONEST_API_KEY,
-      format: "json",
-      results: 1,
-      bucket: 'audio_summary',
-      track_id: spotyfyId
-    }, //Query string data
+    url: 'http://developer.echonest.com/api/v4/song/profile?' + query, //URL to hit
   }, function (error, responce, body) {
     if (error) {
       console.error(error);
       return;
     }
-    console.log('GOT RESPONSE',JSON.parse(body).response);
     var songData = JSON.parse(body).response.songs[0];
     var danceability = songData.audio_summary.danceability;
     console.log('danceability', danceability);
