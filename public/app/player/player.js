21625b9f97b48efdf47f97f7a4b3243060d26739
Brendan Dugan <dugan_brendan@yahoo.com>
2015-08-06 19:56:30 -0700

    [fix] Flesh out html

--- public/app/player/player.js
+++ public/app/player/player.js
@@ -1,5 +1,8 @@
-// this module intantiates the "player" module, and holds the controller for the player and queue.
-// these views will persist across many states of the app, so those states will be subviews of the player
+/** 
+* this module intantiates the "player" module, and holds the controller for the player and queue.
+* these views will persist across many states of the app, so those states will be subviews of the player
+*/
+
 angular.module('turntify.player', [])
 .controller('ProfileController', function (PlayerService) {
   //This will handle the song/queue display logic. Not sure where we will put the numerous hacks
--- public/app/services/sharedServices.js
+++ public/app/services/sharedServices.js
@@ -1,14 +1,17 @@
-//sharedservice.js
-// This will contain information needed throughout the app, and methods to update and relate
-// this information
+/** 
+* This will contain information needed throughout the app, and methods to update and relate
+* this information
+*/
 angular.module('turntify.services')
 .factory('UserService', function($http, $state, $rootScope ) /*might require "RequestService" at some point*/{
   //"persistent data" could include:
   //    -user data (to start: could also be in a "session")
   //    -full song information for queue(i.e spotify track ids, and any other un-displayed information)
+
   //Functions could access different parts of the persistent data needed by different
   //parts of the app: the player view might need app id, but the queue might just list track
   // names, etc.
+  
   //Logouts could be handled here if they aren't handled in a session    
 
   return {
