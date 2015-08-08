0710d632344cfc518843016d30992c6a92fc1998
Brendan Dugan <dugan_brendan@yahoo.com>
2015-08-08 15:03:09 -0700

    [fix] improve routing

--- public/app/login/loginServices.js
+++ public/app/login/loginServices.js
@@ -1,3 +1,8 @@
-angular.module('turntify.login')
-.factory('LoginService', function (RequestService) {
+//NOTE: This currently isn't required by the app
+angular.module('turntify.login', [])
+.controller('LoginController', function (UserService) {
+  this.logout = function(){
+    console.log("logging out! lel");
+    //UserService.logout();
+  }
 });
