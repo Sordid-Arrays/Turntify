9f901cfbed82b6f17334d33b2dbef98e33752a22
mjsgreen01 <mjsgreen01@gmail.com>
2015-08-11 15:33:07 -0700

    first test - not working

--- public/app/player/player.test.js
+++ public/app/player/player.test.js
@@ -1,1 +1,23 @@
 //player.test.js
+
+describe('PlayerController', function(){
+
+  beforeEach(module('turntify.player'));
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  var PlayerController = $controller('PlayerController', { $scope: $scope });
+
+  describe('updatesQueue', function(){
+
+    it('should be a function', function(){
+
+      expect($scope.updatesQueue).to.be.a('function');
+    });
+
+  });
+
+});
