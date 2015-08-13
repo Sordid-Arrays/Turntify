0c8a2e4c96fdd8ccea6071e80433c3a4a0c60250
Brendan Dugan <dugan_brendan@yahoo.com>
2015-08-13 15:43:16 -0700

    [fix] Fix routing from logout state

--- public/app/login/loginServices.js
+++ public/app/login/loginServices.js
@@ -1,8 +1,4 @@
 //NOTE: This currently isn't required by the app
 angular.module('turntify.login', [])
 .controller('LoginController', function (UserService) {
-  this.logout = function(){
-    console.log("logging out! lel");
-    //UserService.logout();
-  }
 });
