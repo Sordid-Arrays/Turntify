f1eef461599009650486f27b82f5e035228a3718
Shu Yoshioka <yoshioka63@gmail.com>
2015-08-18 02:15:40 +0900

    [refactor] optimize api requests

--- helpers/playlistHelper.js
+++ helpers/playlistHelper.js
@@ -1,18 +1,31 @@
+var _ = require('underscore');
+var Promise = require('bluebird');
+
 var spotify = require('../middlewares/spotify.js');
 var echonest = require('../middlewares/echonest.js');
 var util = require('../helpers/util');
-var _ = require('underscore');
 var Songs = require('../models/songs.js');
 var GhettoNest = require('../models/ghettoNest.js');
 
-function getTracks(userId, playlistId, accessToken, refreshToken, tracks, index) {
-  tracks = tracks || [];
-  index = index || 0;
-  var totalTracks = 0;
-  var songUris = [];
-  var dbSongs = [];
+/*
+*  map the spotify URI in the playlist and return an array of URIs
+*/
+function mapUris(playlist) {
+  return _.chain(playlist.items)
+  .map(function(item) {
+    return item.track.uri;
+  })
+  .filter(function(uri) {
+    // do not include URIs start with 'spotify:local', they are not in Echonest
+    return uri.indexOf('spotify:track:') === 0;
+  })
+  .value();
+}
 
-  // getPlaylist tracks from spotify API
+/*
+*  call spotify API to get tracks in the playlist
+*/
+function getSpotifyUris(userId, playlistId, accessToken, refreshToken, index, req) {
   return spotify.getPlaylistTracks(userId, playlistId, accessToken, index)
   .catch(spotify.OldTokenError, function () {
     // statusCode 401:  Unauthorized
@@ -23,72 +36,118 @@ function getTracks(userId, playlistId, accessToken, refreshToken, tracks, index)
       util.saveToken(req, body.access_token, body.refresh_token);
       return spotify.getPlaylistTracks(userId, playlistId, body.access_token);
     });
-  })
+  });
+}
 
-  // get track data from echonest
-  .then(function(playlist) {
-    totalTracks = playlist.total;
-    songUris = _.chain(playlist.items)
-    .map(function(item) {
-      return item.track.uri;
-    })
-    .filter(function(uri) {
-      //this is not to include where foreignkey start with 'spotify:local'
-      return uri.indexOf('spotify:track:') === 0;
-    })
-    .value();
+/*
+*  functions to get data of songs
+*  1) from DB
+*  2) from Echonest API if they are not in DB and save them in DB
+*  after getting data, concat and return them
+*/
+function getEchonestData (spotifyUris) {
+  var tracks = [];
 
-    // return echonest.getTrackData(songUris);
-    // first check database for existing song
-    return GhettoNest.find({spotify_id: { $in: songUris}});
-  }).then(function(songs) {
-    dbSongs = songs;
-    //check which song does not exist in database
+  // 1) check database for existing song
+  return GhettoNest.find({spotify_id: { $in: spotifyUris}})
+  .then(function(dbSongs) {
+    tracks = dbSongs;
     var dbSongUris = _.map(dbSongs, function(dbSong) {
       return dbSong.spotify_id;
     });
-    //songUris will be songs that are not in the database
-    songUris = _.filter(songUris, function(songUri) {
-      return !_.contains(dbSongUris, songUri);
+    // do not request for songs which are in the database
+    spotifyUris = _.filter(spotifyUris, function(spotifyUri) {
+      return !_.contains(dbSongUris, spotifyUri);
     });
-    //request echonest with songs that are not in our db only if songUris is not blank array
-    if (songUris.length > 0) {
-      return echonest.getTrackData(songUris);
+
+    // 2) request echonest with songs
+    if (spotifyUris.length > 0) {
+      console.log(new Date(), 'Request Echonest API ' + spotifyUris.length + ' songs');
+      return echonest.getTrackData(spotifyUris);
     }
     return [];
   })
 
-  // finter by danceability
   .then(function(echonestSongs) {
-    //tracks = tracks.concat(util.danceableFiltering(songs, turntness));
-    // here songs will be song that are return from echonest and not in our db
-    if (echonestSongs.length > 0) {
-      var newGhettoNest = _.map(echonestSongs, function(echonestSong) {
-        return {
-          spotify_id: echonestSong.tracks[0].foreign_id,
-          echonest_id: echonestSong.id,
-          artist_name: echonestSong.artist_name,
-          title: echonestSong.title,
-          danceability: echonestSong.audio_summary.danceability,
-          energy: echonestSong.audio_summary.energy,
-          duration: echonestSong.audio_summary.duration,
-          album_name: echonestSong.tracks[0].album_name,
-          turnt_bucket: util.getTurntness(echonestSong)
-        };
-      });
-      // // here will insert to our db[]
-      console.log('NEW DATA TURNTNESS: ',newGhettoNest[0].turnt_bucket);
-      GhettoNest.create(newGhettoNest);
-      tracks = tracks.concat(newGhettoNest);
+    if (echonestSongs.length < 1) {
+      return tracks;
     }
-    tracks = tracks.concat(dbSongs);
 
-    // if number of tracks is less than 10 and total tracks is greater than 100,
-    // recurse getTracks
-    if (totalTracks > index + 100) {
-      return getTracks(userId, playlistId, accessToken, refreshToken, tracks, index+100);
-    }
+    console.log(new Date(), 'Got Echonest API response: ' + echonestSongs.length + ' songs');
+    var newGhettoNests = _.map(echonestSongs, function(echonestSong) {
+      return {
+        spotify_id: echonestSong.tracks[0].foreign_id,
+        echonest_id: echonestSong.id,
+        artist_name: echonestSong.artist_name,
+        title: echonestSong.title,
+        danceability: echonestSong.audio_summary.danceability,
+        energy: echonestSong.audio_summary.energy,
+        duration: echonestSong.audio_summary.duration,
+        album_name: echonestSong.tracks[0].album_name,
+        turnt_bucket: util.getTurntness(echonestSong)
+      };
+    });
+    // insert to the database
+    GhettoNest.create(newGhettoNests);
+    tracks = tracks.concat(newGhettoNests);
+
     return tracks;
+  });
+}
+
+
+/*
+*  MAIN FUNCTION
+*  get spotify URI and get Echonest data
+*  call spotify API multiple times because spotify API cannot return
+*  more than 100 songs at a time.
+*/
+function getTracks(userId, playlistId, accessToken, refreshToken, req) {
+  console.log(new Date(), 'getTracks START: ');
+  var allTracks = [];  // push tracks when DB response or Echonest response come back
+  var resCount = 0; // keep track of response count and request count
+  var reqCount = 0;
+
+  // wait until all response come back
+  return new Promise(function (resolve, reject) {
+
+    /*
+    * recursive function
+    * keep getting spotify URIs without waiting for spotify API, DB and Echonest API response
+    * until get all songs in the playlist
+    */
+    function makeRequest(index, first) {
+      reqCount ++;
+      getSpotifyUris(userId, playlistId, accessToken, refreshToken, index, req)
+      .then(function (playlist) {
+        console.log(new Date(), 'got spotify URI ' + index + ' ~ ' + (index + 100));
+        var totalTracks = playlist.total;
+
+        // keep calling spotify API without waiting for the response
+        // only on the first call, recurse to get spotify uri of all songs
+        while (first && totalTracks > index + 100) {
+          index += 100;
+          // recurse with second argument false to avoid recursing
+          makeRequest(index, false);
+        }
+
+        return getEchonestData(mapUris(playlist));
+      })
+      .then(function (tracks) {
+        resCount ++;
+        allTracks = allTracks.concat(tracks);
+        // resolve when all requests come back
+        if (resCount === reqCount) {
+          resolve(allTracks);
+          console.log(new Date(), 'getTracks FINISH: return ' + allTracks.length + ' songs');
+        }
+      })
+      .catch(function (err) {
+        reject(err);
+      });
+    }
+
+    makeRequest(0, true);
   });
 }
 
