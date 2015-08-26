a6cbf1d4f1357321eb61fd4e0b9a3a00b3f81504
Ricky Hendrawan <rhendrawan@yahoo.com>
2015-08-26 10:20:28 -0700

    [feat]give name for new playlist imported to spotify

--- public/app/custom_playlist/customPlaylist.js
+++ public/app/custom_playlist/customPlaylist.js
@@ -1,6 +1,6 @@
 angular.module('turntify.player')
   .controller('CustomPlaylistController', function(PlayerService, $scope, turntToFilter){
-  
+
   /**
   * The user's custom playlist is managed here, along with any functions to add or remove songs from WITHIN this view.
   * The listeners ("$scope.$on") listen for changes outside the state and simply tell the playlist to match the playlist in
@@ -8,6 +8,7 @@ angular.module('turntify.player')
   */
   var vm = this;
   vm.customPlaylist = PlayerService.customPlaylist;
+  //vm.newPlaylist = '';
 
   /**
   * A button within the view allows the user to remove songs from the custom playlist.
@@ -31,7 +32,9 @@ angular.module('turntify.player')
   * Perist the playlist to spotify, grabbing the custom name in the input field
   */
   vm.savePlaylist = function(){
-    PlayerService.savePlaylist(vm.name);
+    //PlayerService.savePlaylist(vm.name);
+    PlayerService.savePlaylist(vm.newPlaylist);
+    vm.newPlaylist = '';
   }
 
   /**
@@ -41,7 +44,7 @@ angular.module('turntify.player')
   vm.generateWidget = function(){
     PlayerService.generateWidget(vm.name);
   }
-  
+
   /**
   * When sibling views make changes to custom playlist, they fire an event
   * that lets the customPLaylist view know to update it's view.
