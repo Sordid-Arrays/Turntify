aadd8600fed59d98481c8db49f3f08c8bbc3cb5a
Shu Yoshioka <yoshioka63@gmail.com>
2015-08-29 06:44:54 +0900

    [fix] scroll working

--- public/app/search/searchService.js
+++ public/app/search/searchService.js
@@ -1,26 +1,35 @@
 angular.module('turntify.search')
 .factory('SearchService', function(RequestService) {
-  var candidates = {};
+  var cache = {};
 
   var checkCache = function (input) {
-    if (candidates[input]) {
-      return candidates[input];
+    input = input.trim();
+    if (input.length === 0) {
+      return [];
+    }
+    if (cache[input]) {
+      return cache[input];
     }
     return false;
   };
 
-  var autoComplete = function (input) {
-    var searchWords = input.split(/\s+/);
-    var qs = searchWords.reduce(function (memo, word) {
-      return memo + 'artist=' + word + '&';
-    },'');
-    qs = qs.slice(0, qs.length - 1);
-    // return RequestService.searchTracks(qs)
-    return RequestService.searchArtists(qs)
-    .then(function (tracks) {
-      candidates[input] = tracks;
-      return tracks;
-    });
+  var autoComplete = function (input, callback) {
+    var timeoutId = '';
+    // wait until the user finish typing
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      var searchWords = input.split(/\s+/);
+      var qs = searchWords.reduce(function (memo, word) {
+        return memo + 'artist=' + word + '&';
+      },'');
+      qs = qs.slice(0, qs.length - 1);
+      // return RequestService.searchTracks(qs)
+      return RequestService.searchArtists(qs)
+      .then(function (res) {
+        cache[input] = res;
+        callback(res);
+      });
+    }, 300);
   };
 
   var searchTracks = function (input) {
