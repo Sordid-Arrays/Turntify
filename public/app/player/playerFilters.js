bc413f54c613ade090313ad988f3705f45a6f4f4
Brendan Dugan <dugan_brendan@yahoo.com>
2015-08-29 14:27:37 -0700

    [fix] improve filtering accuracy

--- public/app/player/playerFilters.js
+++ public/app/player/playerFilters.js
@@ -8,20 +8,19 @@ angular.module('turntify.player')
   //the "input" will be a song object from the "playlist" array, and the "turntness" will be
   // the current value of turntness
   //NOTE: selected turntness is a string for some reason
-  var resultArray = [], filterArr = [[],[],[],[]], diff, songBucket, turntness, boundaries;
+  var resultArray = [], filterArr = [[],[],[],[],[]], diff, songBucket, turntness;
   return function(input, selectedTurntness) {
     turntness = parseInt(selectedTurntness);
-    boundaries = [(turntness-1), (turntness+4)];
-    filterArr = [[],[],[],[]];
+    filterArr = [[],[],[],[],[]];
     resultArray = [];
     input.forEach(function(song){
       songBucket = song.turnt_bucket;
-      diff = turntness - songBucket + 1;
-      if(diff === 0 || diff === 1 || diff === 2 || diff === 3){
+      diff = turntness - songBucket + 2;
+      if(diff === 0 || diff === 1 || diff === 2 || diff === 3 || diff == 4){
         filterArr[diff].push(song);
       };
     });
-    resultArray = resultArray.concat(filterArr[1].concat(filterArr[2].concat(filterArr[0].concat(filterArr[3]))));
+    resultArray = resultArray.concat(filterArr[2].concat(filterArr[1].concat(filterArr[3].concat(filterArr[0].concat(filterArr[4])))));
     return resultArray;
   };
 });
