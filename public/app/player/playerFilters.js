d69b4a427bfc339be7f77dc40d12310582dd4cbe
Brendan Dugan <dugan_brendan@yahoo.com>
2015-08-31 12:27:15 -0700

    [docs] write press release

--- public/app/player/playerFilters.js
+++ public/app/player/playerFilters.js
@@ -7,15 +7,24 @@ angular.module('turntify.player')
   
   //the "input" will be a song object from the "playlist" array, and the "turntness" will be
   // the current value of turntness
-  //NOTE: selected turntness is a string for some reason
+  
+  /**
+  * Declare variables in closure in the hopes that browsers don't create additional memory for the same variables.
+  * TODO: find out if this is true. at all.
+  * Data the proper songs will be filtered by best fit: the best fitting songs will be in the middle of the filter array
+  * of arrays, the poorest fits at the end.
+  */
   var resultArray = [], filterArr = [[],[],[],[],[]], diff, songBucket, turntness;
   return function(input, selectedTurntness) {
+    //selectedTurntness is stored as a string, to interact properly with the Angular Material directive
     turntness = parseInt(selectedTurntness);
+    //"filterArr" stores the 5 different categories into which the songs are filtered.
     filterArr = [[],[],[],[],[]];
     resultArray = [];
     input.forEach(function(song){
       songBucket = song.turnt_bucket;
       diff = turntness - songBucket + 2;
+      //Ideally, we are storing the 5 nearest buckets for the user's playlist: 1 would contain 0,1,2,3
       if(diff === 0 || diff === 1 || diff === 2 || diff === 3 || diff == 4){
         filterArr[diff].push(song);
       };
