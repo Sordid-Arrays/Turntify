3a671148bacfad83c7518945c0f5fee89a9b7c61
mjsgreen01 <mjsgreen01@gmail.com>
2015-09-05 13:55:35 -0700

    [refactor] remove getPlaylist func from controller, add test for counter

--- public/app/player/player.js
+++ public/app/player/player.js
@@ -64,17 +64,7 @@ angular.module('turntify.player', ['ngMaterial', 'ngAnimate'])
     var currentlyChecked = filterFilter(vm.playlists, {checked: true});
     PlayerService.updateChecks();
   };
-  /**
-  * Called from view, this takes a stringified version of a selected playlist's description
-  * and calls the  playerService to update playlists
-  */
-  vm.getPlaylist = function(playlistString){
-    var playlist = JSON.parse(playlistString);
-    vm.name = playlist.name;
-    PlayerService.getPlaylist(playlist).then(function(){
-      vm.playlistLoading = false;
-    });
-  };
+
   // Dummy List of Playlists
   // this.playlists = [{"name":"hip hop party","playlistId":"21eoa7xkMxlfjluPVlublp"},{"name":"Minimal // Deep House","playlistId":"2POsysj8dTuOCAMPYVsgeO"},{"name":"Sample","playlistId":"5zA1FfniCXXT2e7gLu8igS"},{"name":"Soul Classics","playlistId":"3q9TPV3tR3sW30ChwNUInG"},{"name":"soul/funk/jazz/afro/latin/60's psychedelia","playlistId":"6iwuMjD6RPaFG0vBENgYaK"},{"name":"Electro Swing","playlistId":"0ZdkjCY51DTu7wjrcReIIl"},{"name":"Neo Disco","playlistId":"0jXuFjs93Tavfsy19s1lk6"},{"name":"Soulfunkisticated","playlistId":"7vKhVBM3kMcuPjtwrHZB8Q"},{"name":"Neo-Disco","playlistId":"3qOoxtnh8AHJV7jQODk9PJ"},{"name":"Gammal Hederlig Reggae","playlistId":"1mxFzXQr7S892OeO4ttz47"},{"name":"All Reggae until 2015 from Roots to Dancehall","playlistId":"3dhsw3G11F9nVbPU72lQiA"},{"name":"Liked from Radio","playlistId":"4MpZz1jhAoV6BSHr1m7i3M"},{"name":"October 1st Show","playlistId":"67nnPuurEMPR6AX1b5MoiB"},{"name":"Starred in Moodsnap","playlistId":"6pjbGz7ryFBRthokhUmQzw"}];
 
--- public/app/player/player.test.js
+++ public/app/player/player.test.js
@@ -7,30 +7,33 @@ describe('PlayerController', function(){
   beforeEach( function() {
     module('turntify.player');
     inject(function(_$rootScope_, _$controller_, _$q_){
-    $q = _$q_;
+      $q = _$q_;
 
-    /**
-    * Mock the player service
-    */ 
-    PlayerServiceMock = {
-      getQueue: function(){
-        return $q(function(resolve, reject){
-          resolve();
-        });
-      },
-      getListOfPlaylists: sinon.spy(function(){
-        return $q(function(resolve, reject){
-          resolve();
-        });
-      }),
-      playlists: [{"name":"hip hop party","playlistId":"21eoa7xkMxlfjluPVlublp"},{"name":"Minimal // Deep House","playlistId":"2POsysj8dTuOCAMPYVsgeO"},{"name":"Sample","playlistId":"5zA1FfniCXXT2e7gLu8igS"},{"name":"Soul Classics","playlistId":"3q9TPV3tR3sW30ChwNUInG"},{"name":"soul/funk/jazz/afro/latin/60's psychedelia","playlistId":"6iwuMjD6RPaFG0vBENgYaK"},{"name":"Electro Swing","playlistId":"0ZdkjCY51DTu7wjrcReIIl"},{"name":"Neo Disco","playlistId":"0jXuFjs93Tavfsy19s1lk6"},{"name":"Soulfunkisticated","playlistId":"7vKhVBM3kMcuPjtwrHZB8Q"},{"name":"Neo-Disco","playlistId":"3qOoxtnh8AHJV7jQODk9PJ"},{"name":"Gammal Hederlig Reggae","playlistId":"1mxFzXQr7S892OeO4ttz47"},{"name":"All Reggae until 2015 from Roots to Dancehall","playlistId":"3dhsw3G11F9nVbPU72lQiA"},{"name":"Liked from Radio","playlistId":"4MpZz1jhAoV6BSHr1m7i3M"},{"name":"October 1st Show","playlistId":"67nnPuurEMPR6AX1b5MoiB"},{"name":"Starred in Moodsnap","playlistId":"6pjbGz7ryFBRthokhUmQzw"}],    
-      queue: [{"title":"Keep Your Head to the Sky","artist_name":"Earth, Wind & Fire","artist_foreign_ids":[{"catalog":"spotify","foreign_id":"spotify:artist:4QQgXkCYTt3BlENzhyNETg"}],"tracks":[{"foreign_release_id":"spotify:album:01c1PLpIdfwy47yid7GqKB","catalog":"spotify","foreign_id":"spotify:track:0iPy1MWCrOT7SjGs14zSSv","id":"TRCIBOE144D0ADEB29"}],"artist_id":"ARVWJM61187B9B7715","id":"SOAZNOA1313438665C","audio_summary":{"key":9,"analysis_url":"http://echonest-analysis.s3.amazonaws.com/TR/kiHWSQRhZwSNtKpG1BE9CrZYA0_ER-…DFEY23UEVW42BQ&Expires=1439403480&Signature=EOpd7FDATQCKabzqS7wTXykcygk%3D","energy":0.523181,"liveness":0.649613,"tempo":177.917,"speechiness":0.047888,"acousticness":0.22946,"instrumentalness":0,"mode":0,"time_signature":4,"duration":309.57333,"loudness":-8.794,"audio_md5":"","valence":0.543813,"danceability":0.349687}}] 
-    };
-    
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $scope = _$rootScope_.$new();
-    $controller = _$controller_;
-    PlayerController = $controller('PlayerController', { $scope: $scope, PlayerService: PlayerServiceMock });
+      /**
+      * Mock the player service
+      */ 
+      PlayerServiceMock = {
+        playlistCounter : 0,
+
+        getQueue: function(){
+          return $q(function(resolve, reject){
+            resolve();
+          });
+        },
+        
+        getListOfPlaylists: sinon.spy(function(){
+          return $q(function(resolve, reject){
+            resolve();
+          });
+        }),
+        playlists: [{"name":"hip hop party","playlistId":"21eoa7xkMxlfjluPVlublp"},{"name":"Minimal // Deep House","playlistId":"2POsysj8dTuOCAMPYVsgeO"},{"name":"Sample","playlistId":"5zA1FfniCXXT2e7gLu8igS"},{"name":"Soul Classics","playlistId":"3q9TPV3tR3sW30ChwNUInG"},{"name":"soul/funk/jazz/afro/latin/60's psychedelia","playlistId":"6iwuMjD6RPaFG0vBENgYaK"},{"name":"Electro Swing","playlistId":"0ZdkjCY51DTu7wjrcReIIl"},{"name":"Neo Disco","playlistId":"0jXuFjs93Tavfsy19s1lk6"},{"name":"Soulfunkisticated","playlistId":"7vKhVBM3kMcuPjtwrHZB8Q"},{"name":"Neo-Disco","playlistId":"3qOoxtnh8AHJV7jQODk9PJ"},{"name":"Gammal Hederlig Reggae","playlistId":"1mxFzXQr7S892OeO4ttz47"},{"name":"All Reggae until 2015 from Roots to Dancehall","playlistId":"3dhsw3G11F9nVbPU72lQiA"},{"name":"Liked from Radio","playlistId":"4MpZz1jhAoV6BSHr1m7i3M"},{"name":"October 1st Show","playlistId":"67nnPuurEMPR6AX1b5MoiB"},{"name":"Starred in Moodsnap","playlistId":"6pjbGz7ryFBRthokhUmQzw"}],    
+        queue: [{"title":"Keep Your Head to the Sky","artist_name":"Earth, Wind & Fire","artist_foreign_ids":[{"catalog":"spotify","foreign_id":"spotify:artist:4QQgXkCYTt3BlENzhyNETg"}],"tracks":[{"foreign_release_id":"spotify:album:01c1PLpIdfwy47yid7GqKB","catalog":"spotify","foreign_id":"spotify:track:0iPy1MWCrOT7SjGs14zSSv","id":"TRCIBOE144D0ADEB29"}],"artist_id":"ARVWJM61187B9B7715","id":"SOAZNOA1313438665C","audio_summary":{"key":9,"analysis_url":"http://echonest-analysis.s3.amazonaws.com/TR/kiHWSQRhZwSNtKpG1BE9CrZYA0_ER-…DFEY23UEVW42BQ&Expires=1439403480&Signature=EOpd7FDATQCKabzqS7wTXykcygk%3D","energy":0.523181,"liveness":0.649613,"tempo":177.917,"speechiness":0.047888,"acousticness":0.22946,"instrumentalness":0,"mode":0,"time_signature":4,"duration":309.57333,"loudness":-8.794,"audio_md5":"","valence":0.543813,"danceability":0.349687}}] 
+      };
+      
+      // The injector unwraps the underscores (_) from around the parameter names when matching
+      $scope = _$rootScope_.$new();
+      $controller = _$controller_;
+      PlayerController = $controller('PlayerController', { $scope: $scope, PlayerService: PlayerServiceMock });
    });
   });
 
@@ -68,7 +71,6 @@ describe('PlayerController', function(){
   describe('init', function(){
 
     it('should call PlayerService.getListOfPlaylists', function(){
-      // var spy = sinon.spy(PlayerService, 'getListOfPlaylists');
       
       expect(PlayerServiceMock.getListOfPlaylists.calledOnce).to.equal(true);
       // expect(PlayerController.updateQueue).to.be.a('function');
@@ -85,6 +87,10 @@ describe('PlayerController', function(){
         return PlayerController.playlists;
       }).should.eventually.equal(PlayerServiceMock.playlists);
       $scope.$digest();
+    });
+
+    it('should update the playlist-counter', function(){
+      expect(PlayerController.playlistCounter).to.equal(0);
     });
 
   });
