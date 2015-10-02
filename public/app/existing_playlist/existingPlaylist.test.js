29a3883b1de356e491e3de86db155e0dafe1bad1
mjsgreen01 <mjsgreen01@gmail.com>
2015-10-02 15:06:47 -0400

    [test] existing-playlist dir. calls toggleCheck

--- public/app/existing_playlist/existingPlaylist.test.js
+++ public/app/existing_playlist/existingPlaylist.test.js
@@ -1,6 +1,6 @@
 
 describe('existingPlaylistDirective', function(){
-  var $scope, $q, $compile, container, mockPlaylist;
+  var $scope, $q, $compile, container, mockPlaylist, PlayerServiceMock;
 
   beforeEach( function() {
     module('turntify.player', function($provide){
@@ -22,12 +22,13 @@ describe('existingPlaylistDirective', function(){
     // load the templates
     module('directiveTemplates');
 
-    inject(function(_$rootScope_, _$compile_, _$q_){
+    inject(function(_$rootScope_, _$compile_, _$q_, _PlayerService_){
 
       // The injector unwraps the underscores (_) from around the parameter names when matching
       $scope = _$rootScope_.$new();
       $q = _$q_;
       $compile = _$compile_;
+      PlayerServiceMock = _PlayerService_;
 
       $scope.mockPlaylist = {'name':'hip hop party','playlistId':'21eoa7xkMxlfjluPVlublp'};
 
@@ -42,12 +43,18 @@ describe('existingPlaylistDirective', function(){
   it('should contain an md-checkbox element', function(){
     var checkbox = container.find('md-checkbox');
     expect(checkbox[0]).to.exist;
-      console.log('from existing playlist ', container);
+    console.log('from existing playlist ', container);
   });
 
   it('should print the playlist name', function(){
     var playlistName = container.find('span').html();
     expect(playlistName).to.have.string('hip hop party');
+  });
+
+  it('should call PlayerService.togglecheck on click', function(){
+    var checkbox = container.find('md-checkbox')[0];
+    checkbox.click();
+    expect(PlayerServiceMock.toggleCheck.calledOnce).to.equal(true);
   });
 
 
